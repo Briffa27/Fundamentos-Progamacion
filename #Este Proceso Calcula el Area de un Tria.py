ef99b17fdb9a089264a2cf987a9036a6189976fd
Briffa27 <146558039+Briffa27@users.noreply.github.com>
2023-10-04 09:54:36 -0400

    Diego Briffa y Kristhian Hernandez
    Actividad 1

---
+++ #Este Proceso Calcula el Area de un Tria.py
@@ -0,0 +1,12 @@
+    #Este Proceso Calcula el Area de un Triangulo
+    #Defino Altura y Base
+Altura: int
+Base: int
+Resultado: float
+    #Pido Datos Al Usuario
+Altura = int (input ("Introduce Altura:"))
+Base = int(input("Introduce Base:")) 
+    #Realizo el Calculo de el area de un triangulo
+Resultado = Altura*Base/2
+    #Muestro el Resultado
+print ("El Resultado es", Resultado)
